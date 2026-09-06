b5ad88859f872ccc9e8560eb85c127e428db0dd2
Kkacper04 <kacpicho@gmail.com>
2026-09-07 01:40:50 +0200

    feat(compatibility): add component compatibility check utility

---
+++ frontend/src/utils/suggest_build.ts
@@ -0,0 +1,45 @@
+import type{ CPUResponse, MotherboardResponse, RAMResponse } from '../types/api';
+
+export type CompatibilityStatus = 'COMPATIBLE' | 'INCOMPATIBLE' | 'NEUTRAL';
+
+export const checkComponentCompatibility = (
+    item: any, 
+    category: string,
+    currentBuild: { cpu?: CPUResponse, motherboard?: MotherboardResponse, ram?: RAMResponse }
+): CompatibilityStatus => {
+    const cat = category.toUpperCase();
+     if (cat === 'CPU') {
+        if (currentBuild.motherboard) {
+            return item.socket === currentBuild.motherboard.socket 
+                ? 'COMPATIBLE' 
+                : 'INCOMPATIBLE';
+        }
+
+    }
+
+    if(cat == 'MOTHERBOARD'){
+        let isSocketOk = true;
+        let isDdrOk = true;
+        let hasAnythingToCompare = false; 
+        if (currentBuild.cpu) {
+            hasAnythingToCompare = true;
+            isSocketOk = item.socket === currentBuild.cpu.socket;
+        }
+        
+        if (currentBuild.ram) {
+            hasAnythingToCompare = true;
+            isDdrOk = item.ddr_generation === currentBuild.ram.ddr_generation;
+        }
+        if (hasAnythingToCompare) {
+            return (isSocketOk && isDdrOk) ? 'COMPATIBLE' : 'INCOMPATIBLE';
+        }
+    }
+     if (cat === 'RAM') {
+        if (currentBuild.motherboard) {
+            return item.ddr_generation === currentBuild.motherboard.ddr_generation 
+                ? 'COMPATIBLE' 
+                : 'INCOMPATIBLE';
+        }
+    }
+    return 'NEUTRAL';
+}
